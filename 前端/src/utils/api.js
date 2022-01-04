9c44e70e2eecf147da9878cbeedbdf131ac746bc
littlePING-ubuntu <13661729587@163.com>
2022-01-04 14:29:03 +0800

    加入前端

---
+++ 前端/src/utils/api.js
@@ -0,0 +1,7 @@
+let api = {
+    get_car_list: () => {[
+        { id: "沪A12345", color: "白色", series: "桑塔纳", type: "轿车" },
+        { id: "沪A23456", color: "蓝色", series: "途安", type: "SUV" },
+      ]}
+}
+export default api
---
+++ 前端/src/views/reg/fix_reg.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="app-container">
+    <el-form ref="form" :model="form" label-width="100px" style="max-width: 1200px">
+      <el-form-item label="车牌号">
+        <el-select v-model="form.car_id" filterable>
+          <el-option v-for="item in car_list" :key="item.id" :value="item.id" :label="item.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="客户号">
+        <el-select v-model="form.client_id" filterable>
+          <el-option v-for="item in client_list" :key="item.id" :value="item.id" :label="item.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="维修类型">
+        <el-radio-group v-model="form.priority">
+          <el-radio-button label="加急" />
+          <el-radio-button label="普通" />
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="作业分类">
+        <el-radio-group v-model="form.type">
+          <el-radio-button label="小修" />
+          <el-radio-button label="中修" />
+          <el-radio-button label="大修" />
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="结算方式">
+        <el-radio-group v-model="form.type">
+          <el-radio-button label="现付" />
+          <el-radio-button label="月结" />
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="进厂时间">
+        <el-date-picker v-model="form.in_time" type="date" />
+      </el-form-item>
+      <el-form-item label="业务员">
+        <el-input:short v-model="form.clerk_name" />
+      </el-form-item>
+      <el-form-item label="业务员编号">
+        <el-input v-model="form.clerk_id" class="short"/>
+      </el-form-item>
+      <el-form-item label="预计完工时间">
+        <el-date-picker v-model="form.est_time" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import '@/styles/index.scss'
+import api from "@/utils/api";
+export default {
+  data() {
+    return {
+      form: {
+        car_id: "",
+        client_id: "",
+        priority: "普通",
+        type: "",
+        pay: "",
+        in_time: "",
+        clerk_name: "",
+        clerk_id: "",
+        est_time: "",
+      },
+      car_list: [],
+      client_list: [],
+    };
+  },
+  mounted() {
+    console.log(123);
+    this.car_list = api.get_car_list();
+    this.client_list = [{ id: "123" }];
+  },
+};
+</script>
