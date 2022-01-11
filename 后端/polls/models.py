80db8c2ad2aa1e7a1bee609fa1c8eae59e2c6912
id-none <id_none@163.com>
2022-01-11 15:36:29 +0800

    后端修改

--- 后端/polls/models.py
+++ 后端/polls/models.py
@@ -4,16 +4,10 @@ from django.db import models
 
 
 class Clients(models.Model):
-    PROPERTY_CHOICES = [
-        ('1', '单位'),
-        ('2', '个人'),
-        ('3', '未指定')
-    ]
     clientId = models.IntegerField(primary_key=True)
     clientName = models.CharField(max_length=16)
     property = models.CharField(
-        max_length=1,
-        choices=PROPERTY_CHOICES,
+        max_length=255,
         default='3'
     )
     linkman = models.CharField(max_length=8)
@@ -21,60 +15,33 @@ class Clients(models.Model):
 
 
 class Cars(models.Model):
-    TYPE_CHOICES = [
-        ('1', '轿车'),
-        ('2', 'SUV'),
-        ('3', 'MPV'),
-        ('4', '跑车'),
-        ('5', '皮卡'),
-        ('6', '其他'),
-        ('7', '未指定')
-    ]
     licensePlate = models.CharField(primary_key=True)
     color = models.CharField(max_length=4)
     brand = models.CharField(max_length=8)
     type = models.CharField(
-        max_length=1,
-        choices=TYPE_CHOICES,
+        max_length=255,
         default='7'
     )
     belonging = models.CharField(max_length=16)
 
 
-class FixTables(models.Model):
-    FIX_TYPE_CHOICES = [
-        ('1', '维护'),
-        ('2', '修理')
-    ]
-    TASK_TYPE_CHOICES = [
-        ('1', '维护'),
-        ('2', '轻度修理'),
-        ('3', '中度修理'),
-        ('4', '重度修理')
-    ]
-    # From: https://zhuanlan.zhihu.com/p/163411392
-    SETTLEMENT_METHODS_CHOICES = [
-        ('1', '自费'),
-        ('2', '自负'),
-        ('3', '分类自负'),
-        ('4', '自付'),
-    ]
+class FixTables(models.Model):  
     fixTableId = models.IntegerField(primary_key=True)
     registerDate = models.DateField()
     licensePlate = models.CharField()
     fixType = models.CharField(
-        max_length=1,
-        choices=FIX_TYPE_CHOICES,
+        max_length=255,
+        # choices=FIX_TYPE_CHOICES,
         default='1'
     )
     taskType = models.CharField(
-        max_length=1,
-        choices=TASK_TYPE_CHOICES,
+        max_length=255,
+        # choices=TASK_TYPE_CHOICES,
         default='2'
     )
     settlementMethods = models.CharField(
-        max_length=1,
-        choices=SETTLEMENT_METHODS_CHOICES,
+        max_length=255
+        # choices=SETTLEMENT_METHODS_CHOICES,
     )
     enterTime = models.DateTimeField()
     salesman = models.CharField(max_length=8)
@@ -83,16 +50,26 @@ class FixTables(models.Model):
 
 
 class MaintenanceMan(models.Model):
-    WORK_TYPE_CHOICES = [
-        ('1', '机修'),
-        ('2', '漆工'),
-        ('3', '焊工')
-    ]
     maintenanceManId = models.IntegerField()
     workType = models.CharField(
-        max_length=1,
-        choices=WORK_TYPE_CHOICES
+        max_length=255
+        # choices=WORK_TYPE_CHOICES
     )
     unitPrice = models.IntegerField()
 
 # 还差项目表和连接表
+
+class ProjectTable(models.Model):
+    projectTableId = models.IntegerField()
+    projectType = models.CharField(
+        max_length=255
+    )
+    workTime=models.IntegerField()
+
+# 连接表
+
+class JoinTables(models.Model):
+    jointableId=models.IntegerField()
+    maintenanceManId = models.IntegerField()
+    numbering= models.IntegerField()
+    workTime=models.CharField()
--- 后端/polls/views.py
+++ 后端/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import NameForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
@@ -12,4 +14,27 @@ def index(request):
 def car(request):
     print('cars')
     print(request.body)
+    if request.method == 'POST':
+        return car_post(request)   
+    elif request.method == 'GET':
+        return car_get(request)
     return HttpResponse('car')
+
+
+def car_post(request):
+    form = NameForm(request.POST)
+    print(form)
+    if form.is_valid():
+        # sql语句
+        return HttpResponse.http.OK
+    
+
+def car_get(request):
+    form = NameForm(request.GET)
+    print(form)
+    if form.is_valid():
+        # sql语句
+        return HttpResponse.http.OK
+    
+    
+            
